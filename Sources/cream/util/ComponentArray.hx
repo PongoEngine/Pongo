a1f271cf4f273688ff959acc67748f5cfd218763
Jeremy <cream@sourcre.am>
2017-11-22 18:21:18 -0800

    add first and last to cArra

--- Sources/cream/util/ComponentArray.hx
+++ Sources/cream/util/ComponentArray.hx
@@ -5,6 +5,17 @@ import haxe.ds.Option;
 @:forward(push, concat, length, iterator)
 abstract ComponentArray<T>(Array<T>) from Array<T>
 {
+
+    public inline function first() : Option<T>
+    {
+        return get(0);
+    }
+
+    public inline function last() : Option<T>
+    {
+        return get(this.length-1);
+    }
+
     public function get(index :Int) : Option<T>
     {
         var val = this[index];
