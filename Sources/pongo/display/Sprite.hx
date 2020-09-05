1039f3f91fc81fb39d77ada1ca65a4f45e182b13
Jeremy Meltingtallow <cream@sourcre.am>
2020-09-05 16:05:23 -0700

    Update Sprite.hx

--- Sources/pongo/display/Sprite.hx
+++ Sources/pongo/display/Sprite.hx
@@ -19,11 +19,86 @@
  * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-package pongo.display;
+import kha.math.FastMatrix3;
+import pongo.display.BlendMode;
+using pongo.math.CMath;
 
-interface Sprite
+class Sprite
 {
-    function draw(graphics :Graphics) : Void;
-    function getNaturalWidth() : Float;
-    function getNaturalHeight() : Float;
+    public var x :Float = 0;
+    public var y :Float = 0;
+    public var anchorX :Float = 0;
+    public var anchorY :Float = 0;
+    public var scaleX :Float = 1;
+    public var scaleY :Float = 1;
+    public var rotation :Float = 0;
+    public var opacity :Float = 1;
+    public var visible :Bool = true;
+    public var blendMode :BlendMode = BlendMode.NORMAL;
+    public var matrix :FastMatrix3 = FastMatrix3.identity();
+
+    public function draw(graphics :Graphics) : Void
+    {
+        
+    }
+
+    public function getNaturalWidth() : Float
+    {
+        return 0;
+    }
+
+    public function getNaturalHeight() : Float
+    {
+        return 0;
+    }
+}
+
+class TransformUtil
+{
+    public static function centerAnchor(transform :Sprite) :Sprite
+    {
+        transform.anchorX = transform.getNaturalWidth()/2;
+        transform.anchorY = transform.getNaturalHeight()/2;
+        return transform;
+    }
+
+    public static function setOpacity(transform :Sprite, opacity :Float) :Sprite
+    {
+        transform.opacity = opacity;
+        return transform;
+    }
+
+    public static function setAnchor(transform :Sprite, x :Float, y :Float) :Sprite
+    {
+        transform.anchorX = x;
+        transform.anchorY = y;
+        return transform;
+    }
+
+    public static function setRotation(transform :Sprite, rotation :Float, fromDegrees :Bool = false) :Sprite
+    {
+        transform.rotation = fromDegrees ? rotation.toRadians() : rotation;
+        return transform;
+    }
+
+    public static function setScale(transform :Sprite, scale :Float) :Sprite
+    {
+        transform.scaleX = scale;
+        transform.scaleY = scale;
+        return transform;
+    }
+
+    public static function setScaleXY(transform :Sprite, scaleX :Float, scaleY :Float) :Sprite
+    {
+        transform.scaleX = scaleX;
+        transform.scaleY = scaleY;
+        return transform;
+    }
+
+    public static function setXY(transform :Sprite, x :Float, y :Float) :Sprite
+    {
+        transform.x = x;
+        transform.y = y;
+        return transform;
+    }
 }
